d44f721d88fdebf154ae00bf85cf79186a05776b
dchan <darren.ccy17@gmail.com>
2025-03-25 09:30:12 +0800

    working (sorta) cowsay

--- main.go
+++ main.go
@@ -5,7 +5,69 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
+
+func tabsToSpaces(lines []string) []string {
+	var ret []string
+	for _, l := range lines {
+		l = strings.Replace(l, "\t", " ", -1) // source: https://pkg.go.dev/strings#Replace
+		ret = append(ret, l)
+	}
+	return ret
+}
+
+func calculateMaxWidth(lines []string) int {
+	w := 0
+	for _, l := range lines {
+		len := utf8.RuneCountInString(l)
+		if len > w {
+			w = len
+		}
+	}
+	return w
+}
+
+func normalizeStringsLength(lines []string, maxwidth int) []string {
+
+	var ret []string
+	for _, l := range lines {
+		s := l + strings.Repeat(" ", maxwidth-utf8.RuneCountInString(l))
+		ret = append(ret, s)
+	}
+	return ret
+}
+
+func buildBalloon(lines []string, maxwidth int) string {
+	var borders []string
+	count := len(lines)
+	var ret []string
+
+	borders = []string{"/", "\\", "\\", "/", "|", "<", ">"}
+
+	top := " " + strings.Repeat("_", maxwidth+2)
+	bottom := " " + strings.Repeat("-", maxwidth+2)
+
+	ret = append(ret, top)
+	if count == 1 {
+		s := fmt.Sprintf("%s %s %s", borders[5], lines[0], borders[6])
+		ret = append(ret, s)
+	} else {
+		s := fmt.Sprintf("%s %s %s", borders[0], lines[0], borders[1])
+		ret = append(ret, s)
+		i := 1
+		for ; i < count-1; i++ {
+			s = fmt.Sprintf("%s %s %s", borders[4], lines[i], borders[4])
+			ret = append(ret, s)
+		}
+		s = fmt.Sprintf("%s %s %s", borders[2], lines[i], borders[3])
+		ret = append(ret, s)
+	}
+
+	ret = append(ret, bottom)
+	return strings.Join(ret, "\n")
+}
 
 func main() {
 	// Get file information about standard input
@@ -22,7 +84,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	// Initialise empty rune slice
-	var output []rune
+	var lines []string
 
 	// Infinite loop
 	for {
@@ -30,12 +92,21 @@ func main() {
 		if err != nil && err == io.EOF {
 			break
 		}
-		output = append(output, input)
+		lines = append(lines, string(input))
 	}
 
-	// Print each unicode character
-	// source: https://pkg.go.dev/fmt
-	for j := 0; j < len(output); j++ {
-		fmt.Printf("%c", output[j])
-	}
+	var cow = `         \  ^__^
+          \ (oo)\_______
+	    (__)\       )\/\
+	        ||----w |
+	        ||     ||
+		`
+
+	lines = tabsToSpaces(lines)
+	maxwidth := calculateMaxWidth(lines)
+	messages := normalizeStringsLength(lines, maxwidth)
+	balloon := buildBalloon(messages, maxwidth)
+	fmt.Println(balloon)
+	fmt.Println(cow)
+	fmt.Println()
 }
